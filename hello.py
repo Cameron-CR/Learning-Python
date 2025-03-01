14b6c97ae4d31f13b618eb264aef8d7d13b228db
Cameron-CR <cameron.ramezanloo@gmail.com>
2025-03-01 15:23:33 -0600

    Initial commit, just did the first part of the course

---
+++ hello.py
@@ -0,0 +1,11 @@
+#This program says Hello and asks for my name
+
+print ("Hello World!")
+print ("What is your name?") # ask for their name
+
+myName = input()
+
+print("It is good to meet you, " + myName)
+print("The legnth of your name is:")
+print(len(myName))
+print("What is your age?")
